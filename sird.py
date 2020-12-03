a7c9daa398f82b1e69c240b93c7d4c942d869345
Nicolò Verardo <nicoloverardo@users.noreply.github.com>
2020-12-03 16:45:19 +0100

    Few parameters fixes

--- sird.py
+++ sird.py
@@ -1,27 +1,27 @@
 import numpy as np
 from scipy.integrate import odeint
 
-def logistic_R0(t, R_0_start=2, k=0.2, x0=40, R_0_end=0.3):
+def logistic_R0(t, R_0_start, k, x0, R_0_end):
     """
     R0 moduled as logistic function
     """
     
     return (R_0_start-R_0_end) / (1 + np.exp(-k*(-t+x0))) + R_0_end
 
-def beta(t, gamma=1/7):
+def beta(t, R_0_start, k, x0, R_0_end, gamma):
     """
     Computes beta at a given time `t`
     """
 
-    return logistic_R0(t) * gamma
+    return logistic_R0(t, R_0_start, k, x0, R_0_end) * gamma
 
-def sird_calc(y, t, N, gamma, alpha, rho, beta):
+def sird_calc(y, t, N, gamma, alpha, rho, R_0_start, k, x0, R_0_end, beta):
     """
     Computes SIRD model
     """
 
     S, I, R, D = y
-    dSdt = -beta(t) * S * I / N
+    dSdt = -beta(t, R_0_start, k, x0, R_0_end, gamma) * S * I / N
     dIdt = -dSdt - (1 - alpha) * gamma * I - alpha * rho * I
     dRdt = (1 - alpha) * gamma * I
     dDdt = alpha * rho * I
@@ -32,7 +32,11 @@ def sird(province,
          gamma=1/7,
          alpha=0.01,
          rho=1/9,
-         days=101):
+         days=101,
+         R_0_start=2, 
+         k=0.2, 
+         x0=40, 
+         R_0_end=0.3):
     """
     Create and compute a SIRD model
 
@@ -57,6 +61,21 @@ def sird(province,
     days : int (default=101)
         Total number of days to predict + 1.
     
+    R_0_start : float (default=2)
+        Starting value of RO
+    
+    k : float (default=0.2)
+        How quickly R0 declines. Lower values of k will
+        let R0 need more time to become lower.
+    
+    x0 : int (default=40)
+        Value on the x-axis of the inflection point of R0.
+        This can be interpreted as the day in which lockdown
+        comes into effect.
+    
+    R_0_end : float (default=0.3)
+        Final value of RO
+    
     Return
     ------
     A numpy array of shape (4, days).
@@ -74,6 +93,11 @@ def sird(province,
     init = N-1, 1, 0, 0
 
     # Solve the model
-    sirsol = odeint(sird_calc, init, times, args=(N, gamma, alpha, rho, beta))
+    sirsol = odeint(sird_calc, init, times, args=(N, gamma,
+                                                  alpha, rho,
+                                                  R_0_start, k,
+                                                  x0, R_0_end,
+                                                  beta
+                                                  ))
 
     return sirsol.T
