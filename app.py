c71292a5c5b1ac57d362f266651fffa29f911570
Nicolò Verardo <nicoloverardo@users.noreply.github.com>
2020-12-03 17:43:46 +0100

    Created dashboard

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('Master Degree Thesis')
+
+st.sidebar.write('Welcome!')
+
+"""
+Welcome to the interactive dashboard of my thesis
+for the MSc in Data Science and Economics at 
+Università degli Studi di Milano.
+"""
+
+st.subheader("Development still in progress...")
