e3641c0c2173bd1ddbd899eb9124b86fdd2cffa4
Nicolò Verardo <nicoloverardo@users.noreply.github.com>
2020-12-06 15:13:16 +0100

    Added selectbox

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ def load_df():
     return load_data(DATA_PATH)
 
 def main():
+    """Main routine of the app"""
     st.set_page_config(
         page_title='Master Degree Thesis - Verardo',
         page_icon='🎓',
@@ -33,7 +34,8 @@ def main():
 
     data_load_state = st.text('Loading data...')
 
-    covidpro_df, dpc_regioni_df, dpc_province_df, pop_prov_df, prov_list_df = load_df()
+    covidpro_df, dpc_regioni_df, dpc_province_df, \
+        pop_prov_df, prov_list_df = load_df()
 
     data_load_state.empty()
 
@@ -50,6 +52,11 @@ def main():
         dpc_province_df[:100]
 
     st.header("Regional plots")
+
+    region_selectbox = st.selectbox(
+        "Region:",
+        dpc_regioni_df.denominazione_regione.unique()
+    )
 
     data_load_state = st.text('Plotting data...')
 
@@ -80,7 +87,10 @@ def main():
     st.plotly_chart(
         custom_plot(
             df=dpc_regioni_df,
-            ydata=['IC_R', 'Hosp_R', 'NC_R_Rolling', 'IC_R_Rolling', 'NC_R', 'NP_R'],
+            ydata=[
+                'IC_R', 'Hosp_R',
+                'NC_R_Rolling', 'IC_R_Rolling',
+                'NC_R', 'NP_R'],
             title='COVID-19 - ',
             xtitle='Date',
             ytitle='Fraction',
@@ -122,7 +132,9 @@ def main():
         custom_plot(
             df=covidpro_df,
             xdata='Date',
-            ydata=['NP_R', 'NP_R_Rolling', 'DR', 'DR_Rolling'],
+            ydata=[
+                'NP_R', 'NP_R_Rolling',
+                'DR', 'DR_Rolling'],
             title='COVID-19 - ',
             xtitle='Date',
             ytitle='Fraction',
@@ -139,6 +151,7 @@ def main():
     use_container_width=True)
 
     data_load_state.empty()
+
 
 if __name__ == "__main__":
     main()
