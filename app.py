c11fde27af939c0ec2e7d8d1ebef71f02c2d04a2
Nicolò Verardo <nicoloverardo@users.noreply.github.com>
2020-12-09 22:40:30 +0100

    Raw data shown before corresp. plot

--- app.py
+++ app.py
@@ -34,29 +34,22 @@ def main():
 
     data_load_state = st.text('Loading data...')
 
-    covidpro_df, dpc_regioni_df, dpc_province_df, \
-        pop_prov_df, prov_list_df = load_df()
+    covidpro_df, dpc_regioni_df, _, _, _ = load_df()
 
     data_load_state.empty()
 
-    if st.sidebar.checkbox('Show raw data'):
-        st.header("Raw data")
-
-        st.subheader("COVIDPro")
-        covidpro_df[:100]
-
-        st.subheader("PCM regions")
-        dpc_regioni_df[:100]
-
-        st.subheader("PCM provinces")
-        dpc_province_df[:100]
+    show_raw_data = st.sidebar.checkbox('Show raw data')
 
     st.header("Regional plots")
 
     region_selectbox = st.selectbox(
         "Region:",
-        dpc_regioni_df.denominazione_regione.unique()
+        dpc_regioni_df.denominazione_regione.unique(),
+        int((dpc_regioni_df.denominazione_regione == 'Lombardia').argmax())
     )
+
+    if show_raw_data:
+        dpc_regioni_df[dpc_regioni_df.denominazione_regione == region_selectbox]  # nopep8
 
     st.plotly_chart(
         custom_plot(
@@ -64,9 +57,12 @@ def main():
             ydata=[
                 'totale_casi',
                 'totale_positivi',
-                'deceduti',
+                'dimessi_guariti',
+                'isolamento_domiciliare',
                 'totale_ospedalizzati',
-                'terapia_intensiva'],
+                'ricoverati_con_sintomi',
+                'terapia_intensiva',
+                'deceduti'],
             title='COVID-19 - ',
             xtitle='Date',
             ytitle='Individuals',
@@ -76,9 +72,12 @@ def main():
             legend_titles=[
                 'Totale Casi',
                 'Totale Positivi',
-                'Deceduti',
+                'Totale Guariti',
+                'Isolamento Domiciliare',
                 'Totale Ospedalizzati',
-                'Terapia Intensiva'],
+                'Ricoverati con sintomi',
+                'Terapia Intensiva',
+                'Deceduti'],
             template='simple_white'
         ), use_container_width=True)
 
@@ -87,7 +86,6 @@ def main():
             df=dpc_regioni_df,
             ydata=[
                 'IC_R', 'Hosp_R',
-                'NC_R_Rolling', 'IC_R_Rolling',
                 'NC_R', 'NP_R'],
             title='COVID-19 - ',
             xtitle='Date',
@@ -98,8 +96,6 @@ def main():
             legend_titles=[
                 'IC over tot. cases',
                 'Hospitalized over tot. cases',
-                'Positives over tampons (rolling avg.)',
-                'IC over tot. cases (rolling avg.)',
                 'Positives over tampons',
                 'Positives over tot. positives'],
             template='simple_white'
@@ -109,16 +105,19 @@ def main():
 
     province_selectbox = st.selectbox(
         "Region:",
-        covidpro_df.Province.unique()
+        covidpro_df.Province.unique(),
+        int((covidpro_df.Province == 'Piacenza').argmax())
     )
+
+    if show_raw_data:
+        covidpro_df[covidpro_df.Province == province_selectbox]
 
     st.plotly_chart(
         custom_plot(
             df=covidpro_df,
             xdata='Date',
             ydata=[
-                'Deaths', 'New_cases',
-                'Tot_deaths', 'Curr_pos_cases'],
+                'Deaths', 'New_cases'],
             title='COVID-19 - ',
             xtitle='Date',
             ytitle='Individuals',
@@ -126,8 +125,7 @@ def main():
             area_name=province_selectbox,
             blend_legend=False,
             legend_titles=[
-                'Deceduti', 'Nuovi casi',
-                'Tot. Morti', 'Totale positivi'],
+                'Deceduti', 'Nuovi casi'],
             template='simple_white'
         ), use_container_width=True)
 
@@ -136,8 +134,23 @@ def main():
             df=covidpro_df,
             xdata='Date',
             ydata=[
-                'NP_R', 'NP_R_Rolling',
-                'DR', 'DR_Rolling'],
+                'Tot_deaths', 'Curr_pos_cases'],
+            title='COVID-19 - ',
+            xtitle='Date',
+            ytitle='Individuals',
+            group_column='Province',
+            area_name=province_selectbox,
+            blend_legend=False,
+            legend_titles=[
+                'Totale deceduti', 'Totale positivi'],
+            template='simple_white'
+        ), use_container_width=True)
+
+    st.plotly_chart(
+        custom_plot(
+            df=covidpro_df,
+            xdata='Date',
+            ydata=['NP_R', 'DR'],
             title='COVID-19 - ',
             xtitle='Date',
             ytitle='Fraction',
@@ -146,9 +159,7 @@ def main():
             blend_legend=True,
             legend_titles=[
                 'Positives over total cases',
-                'Positives over total cases rolling',
-                'Deaths over total cases',
-                'Deaths over total cases rolling'
+                'Deaths over total cases'
                 ],
             template='simple_white'
         ), use_container_width=True)
