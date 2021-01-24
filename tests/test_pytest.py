561c76092dcc1e8deeabc62a739d95510f852c3d
Nicolò Verardo <nicoloverardo@users.noreply.github.com>
2021-01-24 14:47:26 +0100

    Added new tests

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from src.utils import load_data
+from src.sird import beta, logistic_R0
 
 
 def test_data_loading():
@@ -12,3 +13,14 @@ def test_data_loading():
     assert isinstance(dpc_province_df, pd.DataFrame)
     assert isinstance(pop_prov_df, pd.DataFrame)
     assert isinstance(prov_list_df, pd.DataFrame)
+
+
+def test_beta_R0():
+    T = range(1, 10)
+    betas = [beta(t=t, R_0_start=3, k=0.1, x0=20, R_0_end=1, gamma=1/7) for t in T]
+    R0s = [logistic_R0(t=t, R_0_start=3, k=0.1, x0=20, R_0_end=1) for t in T]
+
+    assert betas[0] >= betas[1]
+    assert betas[-1] <= betas[-2]
+    assert R0s[0] >= R0s[1]
+    assert R0s[-1] <= R0s[-2]
