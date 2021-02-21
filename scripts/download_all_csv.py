73c7063d65f343ebe0082c48124697e54ef70012
Nicolò Verardo <nicoloverardo@users.noreply.github.com>
2021-02-21 22:49:03 +0100

    Bugfix

--- scripts/download_all_csv.py
+++ scripts/download_all_csv.py
@@ -1,4 +1,7 @@
-from src.utils import DataDownloader
+import os, sys  # noqa: E401
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+sys.path.insert(0, os.path.abspath('..'))
+from src.utils import DataDownloader  # noqa: E402
 
 if __name__ == "__main__":
     DataDownloader("../data").download_all_csv()
